e1e301d490184807d69ab609614ba5bef9675c41
_ <_>
2023-12-24 11:53:14 +0900

    データ配列のヘッダ表示修正

--- 全銀協フォーマットチェッカー/データセット.cs
+++ 全銀協フォーマットチェッカー/データセット.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace 全銀協フォーマット
 {
@@ -12,6 +14,21 @@ namespace 全銀協フォーマット
             this.ヘッダー = ヘッダー;
             this.データ配列 = データ配列;
             this.トレーラ = トレーラ;
+        }
+        public readonly IEnumerable<Dictionary<string, bool>> データ配列_Keys
+        {
+            get
+            {
+                for (int i = 0; i < データ配列.Max(データ => データ.Count); i++)
+                {
+                    Dictionary<string, bool> Keys = new();
+                    foreach (var Key in データ配列.Where(データ => i < データ.Count).Select(データ => データ.ElementAt(i).Key))
+                    {
+                        Keys[Key] = true;
+                    }
+                    yield return Keys;
+                }
+            }
         }
     }
 }
